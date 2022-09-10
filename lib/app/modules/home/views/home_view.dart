d48a7db318437c8f0a25b08d88b457a9d1bc0d91
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2022-09-10 23:51:13 +0300

    prepare home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
+import '../../../widgets/custom_appbar.dart';
 import '../controllers/home_controller.dart';
+import 'widgets/body.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+    return const SafeArea(
+      child: Scaffold(
+        appBar: CustomAppbar(
+          title: "Jobs Finder",
+          avatarUrl: "https://api.lorem.space/image/face?w=150&h=150",
         ),
+        body: Body(),
       ),
     );
   }
