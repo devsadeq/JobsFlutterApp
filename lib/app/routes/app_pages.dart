d465d09ce55824c2e64709669f0698c124db65c1
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2022-09-11 00:53:44 +0300

    create new routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,11 @@
 import 'package:get/get.dart';
 import 'package:jobs_flutter_app/app/modules/auth/views/register/register_view.dart';
+import 'package:jobs_flutter_app/app/modules/choos/bindings/choos_binding.dart';
+import 'package:jobs_flutter_app/app/modules/choos/views/choos_view.dart';
+import 'package:jobs_flutter_app/app/modules/splash/bindings/splash_binding.dart';
+import 'package:jobs_flutter_app/app/modules/splash/views/splash_view.dart';
+import 'package:jobs_flutter_app/app/modules/waiting/bindings/waiting_binding.dart';
+import 'package:jobs_flutter_app/app/modules/waiting/views/waiting_view.dart';
 
 import '../modules/auth/bindings/auth_binding.dart';
 import '../modules/auth/views/login/login_view.dart';
@@ -11,7 +17,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
@@ -28,6 +34,21 @@ class AppPages {
       name: _Paths.REGISTER,
       page: () => const RegisterView(),
       binding: AuthBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.CHOOSE,
+      page: () => const ChoosView(),
+      binding: ChoosBinding(),
+    ),
+    GetPage(
+      name: _Paths.WAITTING,
+      page: () => const WaitingView(),
+      binding: WaitingBinding(),
     ),
   ];
 }
