63708405455cceb312dd5f26d48a0cd748730f6f
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2022-09-11 03:47:17 +0300

    cleanup choose view

---
+++ lib/app/modules/choose/views/choose_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/choose_controller.dart';
+import 'widgets/body.dart';
+
+class ChooseView extends GetView<ChooseController> {
+  const ChooseView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Get.theme.backgroundColor,
+        body: const Body(),
+      ),
+    );
+  }
+}
