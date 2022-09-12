38632e10111bf4658e7470896397b28acc48cdfd
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2022-09-12 20:24:43 +0300

    create jobs slider

--- lib/app/modules/home/views/widgets/featured_jobs.dart
+++ lib/app/modules/home/views/widgets/featured_jobs.dart
@@ -1,5 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:jobs_flutter_app/app/modules/home/views/widgets/custom_job_card.dart';
 
 import 'section_header.dart';
@@ -12,8 +15,43 @@ class FeaturedJobs extends StatelessWidget {
     return Column(
       children: [
         const SectionHeader(title: "Featured Jobs"),
-        SizedBox(height: 15.h),
-        const CustomJobCard(isFeatured: true),
+        SizedBox(height: 16.h),
+        // const CustomJobCard(isFeatured: true),
+        CarouselSlider(
+          items: const [
+            CustomJobCard(isFeatured: true),
+            CustomJobCard(isFeatured: true),
+            CustomJobCard(isFeatured: true),
+          ],
+          options: CarouselOptions(
+            height: 170.h,
+            aspectRatio: 16 / 9,
+            viewportFraction: 1,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: false,
+            autoPlayInterval: const Duration(seconds: 3),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            scrollDirection: Axis.horizontal,
+          ),
+        ),
+        SizedBox(height: 8.h),
+        DotsIndicator(
+          dotsCount: 3,
+          position: 0,
+          decorator: DotsDecorator(
+            size: Size.square(8.w),
+            activeSize: Size(20.w, 8.w),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(26.r),
+            ),
+            activeColor: Get.theme.colorScheme.secondary,
+            color: const Color(0xffE4E5E7),
+          ),
+        ),
       ],
     );
   }
