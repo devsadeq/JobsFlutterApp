107b8266e5a8f902da7957fe44f8bc2520140d8a
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-09-14 03:35:57 +0300

    start refactor theme

--- lib/app/core/theme/app_theme.dart
+++ lib/app/core/theme/app_theme.dart
@@ -11,8 +11,8 @@ class AppTheme {
   static Color blueColor = const Color(0xff1DA1F2);
   static Color blackColor = const Color(0xff14171A);
   static Color darkGrayColor = const Color(0xff657786);
-  static Color lightGrayColor1 = const Color(0xffAAB8C2);
-  static Color lightGrayColor2 = const Color(0xffE1E8ED);
+  static Color lightGrayColor = const Color(0xffAAB8C2);
+  // static Color lightGrayColor2 = const Color(0xffE1E8ED);
   static Color errorColor = const Color(0xffFB4747);
   static Color whiteColor = const Color(0xffffffff);
   static Color strokeColor = const Color(0xfff2f2f2);
@@ -21,10 +21,10 @@ class AppTheme {
     visualDensity: VisualDensity.adaptivePlatformDensity,
     brightness: Brightness.light,
     backgroundColor: backgroundColor,
-    hintColor: lightGrayColor1,
+    primaryColor: blueColor,
+    hintColor: lightGrayColor,
     cardColor: whiteColor,
     errorColor: errorColor,
-    primaryColor: blackColor,
     textTheme: _lightTextTheme,
     colorScheme: _lightColorScheme,
     elevatedButtonTheme: _lightElevatedButtonTheme,
@@ -39,24 +39,28 @@ class AppTheme {
     caption: GoogleFonts.poppins(
       fontSize: 13.sp,
       fontWeight: FontWeight.w400,
-      color: lightGrayColor1,
+      color: lightGrayColor,
     ),
   );
 
   static final _lightColorScheme = ColorScheme.fromSwatch().copyWith(
-    secondary: blueColor,
-    tertiary: darkGrayColor,
-    onPrimary: lightGrayColor1,
-    onPrimaryContainer: lightGrayColor2,
+    background: backgroundColor,
+    onBackground: blackColor,
+    ///////////////////////////////
+    primary: blueColor,
+    onPrimary: backgroundColor,
+    ///////////////////////////////
+    secondary: darkGrayColor,
+    tertiary: lightGrayColor,
   );
 
   static final _lightElevatedButtonTheme = ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
-      primary: blueColor,
+      backgroundColor: blueColor,
       elevation: 0.0,
       textStyle: _lightTextTheme.button,
       shadowColor: blueColor.withOpacity(0.5),
-      onPrimary: backgroundColor,
+      foregroundColor: backgroundColor,
       padding: EdgeInsets.all(16.w),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(14.r),
