0ae5d525d1cabb81e34acefeee0d9fb64cb9320f
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-09-21 18:47:23 +0300

    use getx bottom sheet

--- lib/app/utils/functions.dart
+++ lib/app/utils/functions.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 import '../widgets/custom_bottom_sheet.dart';
 
 /*
 * Reusable Function that Popup a Bottom Sheet
 * */
-popupBottomSheet(Widget bottomSheetBody) {
-  return showMaterialModalBottomSheet(
-    context: Get.context!,
-    builder: (context) => CustomBottomSheet(body: bottomSheetBody),
-    enableDrag: false,
-    isDismissible: false,
+popupBottomSheet({
+  required Widget bottomSheetBody,
+  bool? isDismissible,
+  bool? enableDrag,
+}) {
+  return Get.bottomSheet(
+    CustomBottomSheet(body: bottomSheetBody),
+    enableDrag: enableDrag ?? true,
+    isDismissible: isDismissible ?? true,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.only(
         topRight: Radius.circular(22.r),
         topLeft: Radius.circular(22.r),
       ),
     ),
+    isScrollControlled: true,
+    backgroundColor: Colors.white,
   );
+  // return showMaterialModalBottomSheet(
+  //   context: Get.context!,
+  //   builder: (context) => CustomBottomSheet(body: bottomSheetBody),
+  //   enableDrag: enableDrag ?? true,
+  //   isDismissible: isDismissible ?? true,
+  //   bounce: true,
+  //   shape: RoundedRectangleBorder(
+  //     borderRadius: BorderRadius.only(
+  //       topRight: Radius.circular(22.r),
+  //       topLeft: Radius.circular(22.r),
+  //     ),
+  //   ),
+  // );
 }
