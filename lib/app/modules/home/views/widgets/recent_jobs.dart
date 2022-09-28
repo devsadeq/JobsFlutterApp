82110d6be0c1fbfe4c6957ed8e21da7e67b54ea1
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-09-28 19:31:12 +0300

    fetch home view jobs

--- lib/app/modules/home/views/widgets/recent_jobs.dart
+++ lib/app/modules/home/views/widgets/recent_jobs.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:get/get.dart';
+import 'package:jobs_flutter_app/app/data/remote/dto/job/job_out_dto.dart';
+import 'package:jobs_flutter_app/app/modules/home/controllers/home_controller.dart';
+import '../../../../utils/constants.dart';
 import '../../../../widgets/custom_job_card.dart';
 import 'section_header.dart';
 
-class RecentJobs extends StatelessWidget {
+class RecentJobs extends GetView<HomeController> {
   const RecentJobs({Key? key}) : super(key: key);
 
   @override
@@ -13,14 +16,33 @@ class RecentJobs extends StatelessWidget {
       children: [
         const SectionHeader(title: "Recent Jobs"),
         SizedBox(height: 16.h),
-        ListView.builder(
-          itemCount: 10,
-          shrinkWrap: true,
-          scrollDirection: Axis.vertical,
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (context, index) => Padding(
-            padding: EdgeInsets.only(bottom: 16.w),
-            child: const CustomJobCard(),
+        controller.rxJobs.when(
+          idle: () => Container(),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          success: (List<JobOutDto>? jobs) => ListView.builder(
+            itemCount: jobs!.length,
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.only(bottom: 16.w),
+              child: CustomJobCard(
+                avatar:
+                    "${AppConstants.BASE_URL}${jobs[index].company!.image!}",
+                companyName: jobs[index].company!.name!,
+                publishTime: jobs[index].created!,
+                jobPosition: jobs[index].position,
+                workplace: jobs[index].workplace,
+                location: jobs[index].location,
+                employmentType: jobs[index].employmentType,
+                isFeatured: false,
+              ),
+            ),
+          ),
+          failure: (String? reason) => Center(
+            child: Text(reason!),
           ),
         )
       ],
