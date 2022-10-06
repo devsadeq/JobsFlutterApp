5accc7beaf731253ec2cf9a8081aea0387b2d920
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-06 14:16:42 +0300

    fix job brief text over flow

--- lib/app/widgets/custom_job_card.dart
+++ lib/app/widgets/custom_job_card.dart
@@ -225,6 +225,7 @@ class _CardJobDescription extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       description,
+      overflow: TextOverflow.ellipsis,
       maxLines: 5,
       style: GoogleFonts.poppins(
           fontSize: 13.sp,
