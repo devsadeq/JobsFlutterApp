24d7aefe7226ce09d66289f41c837d9aa07f2d31
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-23 23:03:41 +0300

    remove app bar

--- lib/app/modules/customerProfile/views/customer_profile_view.dart
+++ lib/app/modules/customerProfile/views/customer_profile_view.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:heroicons/heroicons.dart';
 
-import '../../../widgets/custom_appbar.dart';
 import '../controllers/customer_profile_controller.dart';
 import 'widgets/body.dart';
 
@@ -14,16 +11,6 @@ class CustomerProfileView extends GetView<CustomerProfileController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Get.theme.backgroundColor,
-      appBar: CustomAppBar(
-        leading: IconButton(
-          onPressed: () => Get.back(),
-          icon: const HeroIcon(HeroIcons.chevronLeft),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {}, icon: const HeroIcon(HeroIcons.pencilSquare))
-        ],
-      ),
       body: const Body(),
     );
   }
