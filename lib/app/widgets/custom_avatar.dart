e5d81f0b67b1974b987d0cc9e661f0a7d4e4d1da
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-24 20:01:28 +0300

    edit custom avatar widget

--- lib/app/widgets/custom_avatar.dart
+++ lib/app/widgets/custom_avatar.dart
@@ -16,16 +16,39 @@ class CustomAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(radius ?? 10000.r),
-      child: CachedNetworkImage(
-        imageUrl: imageUrl,
-        placeholder: (context, url) => const Center(
+    return CachedNetworkImage(
+      imageUrl: imageUrl,
+      height: height ?? 80.h,
+      imageBuilder: (context, imageProvider) => Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(
+            image: imageProvider,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
+      placeholder: (context, url) => Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.grey[300],
+        ),
+        child: const Center(
           child: CircularProgressIndicator(),
         ),
-        errorWidget: (context, url, error) =>
-            const HeroIcon(HeroIcons.exclamationCircle),
-        height: height ?? 84.h,
+      ),
+      errorWidget: (context, url, error) => Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.grey[300],
+        ),
+        child: Center(
+          child: HeroIcon(
+            HeroIcons.exclamationCircle,
+            color: Colors.grey[500],
+            size: 0.5 * (height ?? 80.h),
+          ),
+        ),
       ),
     );
   }
