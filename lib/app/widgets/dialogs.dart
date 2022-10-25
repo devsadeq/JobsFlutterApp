50a81b1bb13fd3cfca332c4e37051ff930c3d577
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-25 23:33:59 +0300

    create new dialog

--- lib/app/widgets/dialogs.dart
+++ lib/app/widgets/dialogs.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../core/values/strings.dart';
+import 'custom_lottie.dart';
 
 class Dialogs {
   static Future<bool>? warningDialog({
@@ -30,6 +31,8 @@ class Dialogs {
     required String description,
     String? btnOkText,
     required Function() btnOkOnPress,
+    bool? dismissOnBackKeyPress,
+    bool? dismissOnTouchOutside,
   }) {
     AwesomeDialog(
       context: Get.overlayContext!,
@@ -39,6 +42,8 @@ class Dialogs {
       desc: description,
       btnOkText: btnOkText ?? btnOkText,
       btnOkOnPress: btnOkOnPress,
+      dismissOnBackKeyPress: dismissOnBackKeyPress ?? true,
+      dismissOnTouchOutside: dismissOnTouchOutside ?? true,
       headerAnimationLoop: false,
       dialogBorderRadius: BorderRadius.circular(16.r),
       buttonsBorderRadius: BorderRadius.circular(14.r),
@@ -137,6 +142,51 @@ class Dialogs {
       buttonsTextStyle: GoogleFonts.poppins(
         fontSize: 13.sp,
         fontWeight: FontWeight.w700,
+      ),
+    ).show();
+  }
+
+  static Future<bool>? spaceDialog({
+    String? description,
+    String? title,
+    required Function() btnOkOnPress,
+    bool? dismissOnBackKeyPress,
+    bool? dismissOnTouchOutside,
+  }) {
+    AwesomeDialog(
+      context: Get.overlayContext!,
+      dialogType: DialogType.noHeader,
+      animType: AnimType.bottomSlide,
+      btnOkText: "Retry",
+      btnOkOnPress: btnOkOnPress,
+      dismissOnBackKeyPress: dismissOnBackKeyPress ?? true,
+      dismissOnTouchOutside: dismissOnTouchOutside ?? true,
+      headerAnimationLoop: false,
+      dialogBorderRadius: BorderRadius.circular(16.r),
+      buttonsBorderRadius: BorderRadius.circular(14.r),
+      padding: EdgeInsets.only(bottom: 12.w),
+      btnOkColor: Get.theme.primaryColor,
+      buttonsTextStyle: GoogleFonts.poppins(
+        fontSize: 13.sp,
+        fontWeight: FontWeight.w700,
+      ),
+      body: CustomLottie(
+        title: title ?? "Oops!",
+        description: description,
+        asset: "assets/space.json",
+        padding: EdgeInsets.zero,
+        repeat: true,
+        assetHeight: 200.h,
+        titleStyle: GoogleFonts.poppins(
+          fontSize: 16.sp,
+          fontWeight: FontWeight.w600,
+          color: Get.theme.colorScheme.onBackground,
+        ),
+        descriptionStyle: GoogleFonts.poppins(
+          fontSize: 13.sp,
+          fontWeight: FontWeight.w400,
+          color: Get.theme.colorScheme.secondary,
+        ),
       ),
     ).show();
   }
