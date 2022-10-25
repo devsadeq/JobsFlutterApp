8ec55f1b31990042980ed84f88a9e3e6a8b1747b
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-25 23:59:48 +0300

    create company profile error dialog

--- lib/app/modules/companyProfile/views/widgets/body.dart
+++ lib/app/modules/companyProfile/views/widgets/body.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../../../widgets/custom_lottie.dart';
 import '../../../../widgets/shimmer/company_profile_shimmer.dart';
 import '../../controllers/company_profile_controller.dart';
 import 'company_profile_sliver_app_bar.dart';
@@ -15,52 +14,49 @@ class Body extends GetView<CompanyProfileController> {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => controller.rxCompany.when(
-          idle: () => Container(),
-          loading: () => const CompanyProfileShimmer(),
-          success: (company) => NestedScrollView(
-            headerSliverBuilder: (
-              BuildContext context,
-              bool innerBoxIsScrolled,
-            ) {
-              return <Widget>[
-                CompanyProfileSliverAppBar(company: company!),
-                SliverPersistentHeader(
-                  delegate: SliverPersistentHeaderDelegateImp(
-                    tabBar: TabBar(
-                      controller: controller.tabController,
-                      labelStyle: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      unselectedLabelStyle: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        fontWeight: FontWeight.w700,
-                      ),
-                      labelColor: Get.theme.colorScheme.onPrimary,
-                      unselectedLabelColor: Get.theme.colorScheme.secondary,
-                      indicator: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.r),
-                        color: Get.theme.colorScheme.primary,
-                      ),
-                      tabs: const [
-                        Tab(text: "About us"),
-                        Tab(text: "Jobs"),
-                      ],
+    return Obx(
+      () => controller.rxCompany.when(
+        idle: () => Container(),
+        loading: () => const CompanyProfileShimmer(),
+        failure: (err) => const CompanyProfileShimmer(),
+        success: (company) => NestedScrollView(
+          headerSliverBuilder: (
+            BuildContext context,
+            bool innerBoxIsScrolled,
+          ) {
+            return <Widget>[
+              CompanyProfileSliverAppBar(company: company!),
+              SliverPersistentHeader(
+                delegate: SliverPersistentHeaderDelegateImp(
+                  tabBar: TabBar(
+                    controller: controller.tabController,
+                    labelStyle: GoogleFonts.poppins(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w700,
                     ),
+                    unselectedLabelStyle: GoogleFonts.poppins(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w700,
+                    ),
+                    labelColor: Get.theme.colorScheme.onPrimary,
+                    unselectedLabelColor: Get.theme.colorScheme.secondary,
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12.r),
+                      color: Get.theme.colorScheme.primary,
+                    ),
+                    tabs: const [
+                      Tab(text: "About us"),
+                      Tab(text: "Jobs"),
+                    ],
                   ),
-                  pinned: true,
                 ),
-              ];
-            },
-            body: const CompanyTabView(),
-          ),
-          failure: (e) => CustomLottie(
-            asset: "assets/space.json",
-            repeat: true,
-            title: e!,
-            onTryAgain: controller.onRetry,
-          ),
-        ));
+                pinned: true,
+              ),
+            ];
+          },
+          body: const CompanyTabView(),
+        ),
+      ),
+    );
   }
 }
