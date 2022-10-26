d9d7b4fda6c3e66cc1ebb47d584f5b5b4aed0a5c
Sadeq Al-Mhana <dev.sadiqsalam@gmail.com>
2022-10-26 18:19:05 +0300

    make login page responsive

--- lib/app/modules/auth/views/login/widgets/body.dart
+++ lib/app/modules/auth/views/login/widgets/body.dart
@@ -16,25 +16,26 @@ class Body extends GetView<AuthController> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 29.w),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            SizedBox(height: 118.h),
-            const Header(title: AppStrings.welcomeBack),
-            SizedBox(height: 77.h),
-            const LoginForm(),
-            SizedBox(height: 77.h),
-            ButtonWithText(
-              btnLabel: AppStrings.loginBtn,
-              firstTextSpan: AppStrings.youDoNotHaveAnAccountYet,
-              secondTextSpan: AppStrings.signup,
-              onTap: () => controller.onLoginSubmit(),
-              onTextTap: () => Get.toNamed(Routes.CHOOSE),
-            ),
-          ],
-        ),
-      ),
+      child: CustomScrollView(slivers: [
+        SliverFillRemaining(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Header(title: AppStrings.welcomeBack),
+              SizedBox(height: 30.h),
+              const LoginForm(),
+              SizedBox(height: 50.h),
+              ButtonWithText(
+                btnLabel: AppStrings.loginBtn,
+                firstTextSpan: AppStrings.youDoNotHaveAnAccountYet,
+                secondTextSpan: AppStrings.signup,
+                onTap: () => controller.onLoginSubmit(),
+                onTextTap: () => Get.toNamed(Routes.CHOOSE),
+              ),
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
